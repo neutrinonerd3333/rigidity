1a71fa533fed2eb9d2d7200bc19ecaaf2d6ce951
Tony Zhang <txz@mit.edu>
2017-03-16 11:42:09 -0400

    Fix for case where configuration lies in lower-dimensional hyperplane; rigidity matrix code fix

--- inf_rigidity_testing.py
+++ inf_rigidity_testing.py
@@ -1,5 +1,12 @@
 import numpy
 import numpy.linalg
+import scipy.special
+
+def displacement(start, finish):
+	"""
+	The vector difference finish - start.
+	"""
+	return numpy.array(finish) - numpy.array(start)
 
 def inf_dof(vertex_config, edges):
 	"""
@@ -11,24 +18,38 @@ def inf_dof(vertex_config, edges):
 		   specified as integer pairs (vertex1, vertex2)
 		   that index into vertex_config
 	"""
+
 	embed_dimension = len(vertex_config[0])
 	vertex_n = len(vertex_config)
+	vertex_vectors = numpy.array(vertex_config)
 
 	# we build up the rigidity matrix row by row
 	rigidity_mat_rows = []
 	for (left, right) in edges:
-		edge_displacement = \
-			numpy.array(vertex_config[left]) - numpy.array(vertex_config[right])
+		edge_displacement = vertex_vectors[right] - vertex_vectors[left]
 
 		rigidity_mat_row = numpy.zeros(vertex_n * embed_dimension)
 		left_ind = embed_dimension * left
 		right_ind = embed_dimension * right
 		rigidity_mat_row[left_ind:left_ind+embed_dimension] = edge_displacement
-		rigidity_mat_row[right_ind:right_ind+embed_dimension] = edge_displacement
+		rigidity_mat_row[right_ind:right_ind+embed_dimension] = -edge_displacement
 		
 		rigidity_mat_rows.append(rigidity_mat_row)
-
-	# n*d - rank = nullity = \binom{d+1}{2} + dof
 	rmat_rank = numpy.linalg.matrix_rank(rigidity_mat_rows)
-	plane_isometries_dim = (embed_dimension + 1) * embed_dimension // 2
-	return embed_dimension*vertex_n - rmat_rank - plane_isometries_dim
+
+	# dimension k of lowest-dimensional hyperplane containing all vertices
+	displacements = vertex_vectors - vertex_vectors[0]
+	configuration_dim = numpy.linalg.matrix_rank(displacements)
+
+	# n*d - rank = nullity = (dim infinitesimal rigid motions) + dof
+	# dim infinitesimal rigid motions = dim SO(d) - dim symmetry group of vertices
+	# dim symmetry group of vertices = dim SO(d - k)
+	# dim SO(m) = m+1 choose 2
+
+	euclidean_isometries_dim = scipy.special.binom(embed_dimension + 1, 2)
+	symmetry_group_dim = scipy.special.binom(embed_dimension - configuration_dim, 2)
+
+	return embed_dimension*vertex_n \
+			- rmat_rank \
+			- euclidean_isometries_dim \
+            + symmetry_group_dim
