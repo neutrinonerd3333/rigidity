f2fe270c5cb4daf1834708200b745ecc3774b981
Tony Zhang <txz@mit.edu>
2017-03-16 13:57:18 -0400

    Better test documentation, more tests

--- inf_rigidity_testing_test.py
+++ inf_rigidity_testing_test.py
@@ -22,23 +22,61 @@ class RigidityMatrixTest(unittest.TestCase):
                                       expected_rigiditiy_matrix)
 
 class InfinitesimalDofTest(unittest.TestCase):
+    """
+    Partitions:
+
+    vertices: 0, 1, 2, many
+    edges: 0, 1, many
+    infinitesimal dof: 0, 1, many;
+                       greater than actual dof?
+    embedding dimension d: 1, 2, many
+    configuration dimension k: 0, 1, 1<k<d-1, d-1, d
+    """
+
+    def test1DLine(self):
+        # V = 2, E = 1, DOF = 0, d = 1, k = d
+        config = [(14.2,), (2.991,)]
+        edges = [(0,1)]
+        self.assertEqual(inf_dof(config, edges), 0)
+
+    def testEmpty(self):
+        # V = 0, E = 0, DOF = 0, d = ?, k = ?
+        self.assertEqual(inf_dof([], []), 0)
+
+    def testPoint(self):
+        # V = 1, E = 0, DOF = 0, d = 2, k = 0 < d-1
+        config = [(19, 34.1245)]
+        edges = []
+        self.assertEqual(inf_dof(config, edges), 0)
+
     def testLine(self):
+        # V = 2, E = 1, DOF = 0, d = 2, k = d-1
         config = [(0,0),(1,0)]
         edges = [(0,1)]
         self.assertEqual(inf_dof(config, edges), 0)
 
     def testTriangle(self):
+        # V = many, E = many, DOF = 0, d = 2, k = d
         triangle_config = [(0,0), (1,0), (0,1)]
         edges = [(0,1), (1,2), (2,0)]
         self.assertEqual(inf_dof(triangle_config, edges), 0)
 
     def testSquare(self):
+        # V = many, E = many, DOF = 1, d = 2, k = d
         square_config = [(0,0),(0,5),(5,5),(5,0)]
         edges = [(0,1), (1,2), (2,3), (3,0)]
         self.assertEqual(inf_dof(square_config, edges), 1)
 
+    def testDegenerateRectangle(self):
+        # V = many, E = many, DOF = 4 > actual, d = 3, k = 1 < d-1
+        degen_rect_config = [(0, 0, 5), (3, 0, 5), (4, 0, 5), (1, 0, 5)]
+        edges = [(0,1),(1,2),(2,3),(3,0)]
+        self.assertEqual(inf_dof(degen_rect_config, edges), 4)
+
     def testTwoSquares(self):
         """
+        V = many, E = many, DOF = 2, d = 2, k = d
+
         1--3--5
         |  |  |
         0--2--4
@@ -48,6 +86,7 @@ class InfinitesimalDofTest(unittest.TestCase):
         self.assertEqual(inf_dof(two_square_config, edges), 2)
 
     def testSpokes(self):
+        # V = many, E = many, DOF = many, d = 2, k = d
         n_spokes = 50
         angle = 2 * math.pi / n_spokes
         spoke_config = [(0,0)] + [(math.cos(k * angle), math.sin(k * angle))
